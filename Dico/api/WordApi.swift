f592d1417ade2d152c93e90da9397feaed5205f1
Jaegle6u <loic.jaegle@icloud.com>
2022-03-01 17:43:17 +0100

    Fin premiere seance

---
+++ Dico/api/WordApi.swift
@@ -0,0 +1,8 @@
+//
+//  WordApi.swift
+//  Dico
+//
+//  Created by Loic Jaegle on 01/03/2022.
+//
+
+import Foundation
---
+++ Dico/model/Word.swift
@@ -0,0 +1,8 @@
+//
+//  Word.swift
+//  Dico
+//
+//  Created by Loic Jaegle on 01/03/2022.
+//
+
+import Foundation
